de77507feba94df6039b65739cfcaa23879f9e65
Gustavo <gustavo.prog40@gmail.com>
2021-04-20 17:26:52 -0300

    indo

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,5 @@
 import Head from 'next/head'
-import Header from '../components/Header'
 
 export default function Home() {
-  return (
-    <div>
-      <Header />
-    </div>
-  )
+  return <div></div>
 }
