8dd057b19a464357710aa3e05363959e70aaab8d
Gustavo <gustavo.prog40@gmail.com>
2021-04-22 22:36:53 -0300

    final

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -1,6 +1,9 @@
 import { useContext } from 'react'
+import Slider from 'rc-slider'
+import 'rc-slider/assets/index.css'
 import { PlayerContext } from '../../contexts/PlayerContext'
 import styles from './styles.module.scss'
+import Image from 'next/image'
 
 export default function Player() {
   const { episodeList, currentEpisodeIndex } = useContext(PlayerContext)
@@ -10,33 +13,52 @@ export default function Player() {
     <div className={styles.playerContainer}>
       <header>
         <img src="/playing.svg" alt="playing" />
-        <strong>Tocando Agora {episode?.title}</strong>
+        <strong>Tocando Agora </strong>
       </header>
-      <div className={styles.emptyPlayer}>
-        <strong>Selecione um podcast para ouvir</strong>
-      </div>
-      <footer className={styles.empty}>
+
+      {episode ? (
+        <div className={styles.currentEpisode}>
+          <Image width={592} height={592} src={episode.thumbnail} objectFit="cover" />
+
+          <strong>{episode.title}</strong>
+          <br />
+          <span>{episode.members}</span>
+        </div>
+      ) : (
+        <div className={styles.emptyPlayer}>
+          <strong>Selecione um podcast para ouvir</strong>
+        </div>
+      )}
+      <footer className={!episode ? styles.empty : ''}>
         <div className={styles.progress}>
           <span>00:00</span>
           <div className={styles.slider}>
-            <div className={styles.emptySlider} />
+            {episode ? (
+              <Slider
+                trackStyle={{ backgroundColor: '#04d361' }}
+                railStyle={{ backgroundColor: '#9f75ff' }}
+                handleStyle={{ borderColor: '#04d361', borderWidth: 4 }}
+              />
+            ) : (
+              <div className={styles.emptySlider} />
+            )}
           </div>
           <span>00:00</span>
         </div>
         <div className={styles.buttons}>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/shuffle.svg" alt="embaralhar" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-previous.svg" alt="tocar-anterior" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play.svg" className={styles.playButton} alt="repetir" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/play-next.svg" className={styles.playButton} alt="repetir" />
           </button>
-          <button type="button">
+          <button type="button" disabled={!episode}>
             <img src="/repeat.svg" className={styles.playButton} alt="repetir" />
           </button>
         </div>
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -16,7 +16,7 @@ function MyApp({ Component, pageProps }) {
   }
 
   return (
-    <PlayerContext.Provider value={{ episodeList: [], currentEpisodeIndex: 0, play }}>
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, play }}>
       <div className={styles.wrapper}>
         <main>
           <Header />
